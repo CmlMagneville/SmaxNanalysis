aa6ffff4a1087bc70bc63e89dc4af31ebf0a5865
Cml_Magneville <camille.magneville@gmail.com>
2022-03-10 15:40:28 +0100

    :green_heart: Begin the fct to prepare data for SmaxN increasing timespans

--- R/7_SmaxN_time_functions.R
+++ R/7_SmaxN_time_functions.R
@@ -39,4 +39,48 @@ create.abund.list.timespan <- function(spans_set,
 
 
 
+  # create the abundance list which will contain for each species ...
+  # ... one df per studied tiemspan:
+  abund_list_final <- list()
+
+
+  # for all species:
+  for (i in (1:length(abund_cam_allposes_list))) {
+
+
+    # create a list that will contain new abundance data for each species:
+    abund_time_sp_list <- list()
+
+
+    # create three df refering each to one pose and gather them in a list:
+    pose <- abund_cam_allposes_list[[i]]
+    pose1 <- pose[which(rownames(pose) %in% as.character(hms::as_hms(c(hms::as_hms("07:30:00"):hms::as_hms("08:30:00"))))), ]
+    pose2 <- pose[which(rownames(pose) %in% as.character(hms::as_hms(c(hms::as_hms("11:30:00"):hms::as_hms("12:30:00"))))), ]
+    pose3 <- pose[which(rownames(pose) %in% as.character(hms::as_hms(c(hms::as_hms("15:30:00"):hms::as_hms("16:30:00"))))), ]
+    pose_abund_list <- list(pose1, pose2, pose3)
+
+    # loop on the three poses (as the timespan can not be within two poses ...
+    # ... ex 5 minutes in Pose1 and 5 minutes in Pose2):
+    for (j in (1:length(pose_abund_list))) {
+
+
+      # create a list that will contains new data for a given pose
+      abund_time_pose_list <- list()
+
+
+      # loop on the timespans:
+      for (k in (1:length(spans_set))) {
+
+
+
+
+      } # end loop on tiemspans
+
+    } # end loop on poses
+
+
+  } # end loop on species
+
+
+
 }
