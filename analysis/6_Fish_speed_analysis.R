3960ab60ea8dd5c338533b92fad918452386c8a0
Cml_Magneville <camille.magneville@gmail.com>
2022-06-24 15:20:23 +0200

    :green_heart: Update script speeds and graph

--- analysis/6_Fish_speed_analysis.R
+++ analysis/6_Fish_speed_analysis.R
@@ -43,62 +43,18 @@ colnames(maxN_all) <- c("species_nm", "pose_nb", "maxN", "SmaxN", "SmaxN_timeste
 
 
 
-## Species 1: Ac_Cten_Dark: j'ai essaye pose apres pose:
-
-# Pose 1 voir si ça marche:
-abund_df <- paral_list[[1]]$abund_df
-dist_df <- paral_list[[1]]$dist_df
-speed <- paral_list[[1]]$speed
-
-#start <- Sys.time()
-# Ac_Cten_P1 <- SmaxN::SmaxN.computation(abund_df, speed, dist_df)
-# stop <- Sys.time()
-# time_taken_Ac_Cten_P1 <- stop - start
-
-# ICRS 9 cameras prend 3min30
-
-
-# Pose 2:
-abund_df <- paral_list[[2]]$abund_df
-dist_df <- paral_list[[2]]$dist_df
-speed <- paral_list[[2]]$speed
+## Species 1: Ac_Cten_Dark:
 
 # start <- Sys.time()
-# Ac_Cten_P2 <- SmaxN::SmaxN.computation(abund_df, speed, dist_df)
+# SmaxN_AcCten <- automat.maxN.spbysp(species_nm = "Ac_Cten_dark",
+#                                     abund_list = clean_abund_list,
+#                                     dist_df = dist_df,
+#                                     fish_speed = 1,
+#                                     os = "windows",
+#                                     nb_cores = 3)
 # stop <- Sys.time()
-# time_taken_Ac_Cten_P2 <- stop - start
-
-# ICRS avec 9 cameras: prend: 3h30
-
-
-# Pose 3:
-abund_df <- paral_list[[3]]$abund_df
-dist_df <- paral_list[[3]]$dist_df
-speed <- paral_list[[3]]$speed
-
-# start <- Sys.time()
-# Ac_Cten_P3 <- SmaxN::SmaxN.computation(abund_df, speed, dist_df)
-# stop <- Sys.time()
-# time_taken_Ac_Cten_P3 <- stop - start
-# saveRDS(Ac_Cten_P3, here::here("transformed_data", "Ac_Cten_P3.rds"))
-
-# ICRS 9 camera prend 12 minutes
-
-# combine all of them for ICRS:
-# SmaxN_AcCten <- list(Ac_Cten_P1, Ac_Cten_P2, Ac_Cten_P3)
-
-
-# essaye en parallelisant:
-start <- Sys.time()
-SmaxN_AcCten <- automat.maxN.spbysp(species_nm = "Ac_Cten_dark",
-                                    abund_list = clean_abund_list,
-                                    dist_df = dist_df,
-                                    fish_speed = 0.5,
-                                    os = "windows",
-                                    nb_cores = 3)
-stop <- Sys.time()
-time_taken_AcCten <- stop - start
-saveRDS(SmaxN_AcCten, here::here("transformed_data", "SmaxN_AcCten_raw.rds"))
+# time_taken_AcCten <- stop - start
+# saveRDS(SmaxN_AcCten, here::here("transformed_data", "SmaxN_AcCten_raw_speed2.rds"))
 
 # ... h?
 
@@ -109,12 +65,12 @@ saveRDS(SmaxN_AcCten, here::here("transformed_data", "SmaxN_AcCten_raw.rds"))
 # SmaxN_GC <- automat.maxN.spbysp(species_nm = "Gomphosus_caeruleus",
 #                                 abund_list = clean_abund_list,
 #                                 dist_df = dist_df,
-#                                 fish_speed = 0.5,
+#                                 fish_speed = 1,
 #                                 os = "windows",
 #                                 nb_cores = 3)
 # stop <- Sys.time()
 # time_taken_GC <- stop - start
-# saveRDS(SmaxN_GC, here::here("transformed_data", "SmaxN_GC_raw.rds"))
+# saveRDS(SmaxN_GC, here::here("transformed_data", "SmaxN_GC_raw_speed2.rds"))
 
 # 3.78 heures
 
@@ -125,12 +81,12 @@ saveRDS(SmaxN_AcCten, here::here("transformed_data", "SmaxN_AcCten_raw.rds"))
 # SmaxN_CT <- automat.maxN.spbysp(species_nm = "Chaetodon_trifasciatus",
 #                                 abund_list = clean_abund_list,
 #                                 dist_df = dist_df,
-#                                 fish_speed = 0.5,
+#                                 fish_speed = 1,
 #                                 os = "windows",
 #                                 nb_cores = 3)
 # stop <- Sys.time()
 # time_taken_CT <- stop - start
-# saveRDS(SmaxN_CT, here::here("transformed_data", "SmaxN_CT_raw.rds"))
+# saveRDS(SmaxN_CT, here::here("transformed_data", "SmaxN_CT_raw_speed2.rds"))
 
 # 5 min
 
@@ -141,48 +97,101 @@ saveRDS(SmaxN_AcCten, here::here("transformed_data", "SmaxN_AcCten_raw.rds"))
 # SmaxN_PM <- automat.maxN.spbysp(species_nm = "Parupeneus_macronemus",
 #                                 abund_list = clean_abund_list,
 #                                 dist_df = dist_df,
-#                                 fish_speed = 0.5,
+#                                 fish_speed = 1,
 #                                 os = "windows",
 #                                 nb_cores = 3)
 # stop <- Sys.time()
 # time_taken_PM <- stop - start
-# saveRDS(SmaxN_PM, here::here("transformed_data", "SmaxN_PM_raw.rds"))
+# saveRDS(SmaxN_PM, here::here("transformed_data", "SmaxN_PM_raw_speed2.rds"))
 
 # 19 min
 
 
 ## Species 5 - Thalassoma hardwicke:
 
-start <- Sys.time()
-SmaxN_TH <- automat.maxN.spbysp(species_nm = "Thalassoma_hardwicke",
-                                abund_list = clean_abund_list,
-                                dist_df = dist_df,
-                                fish_speed = 0.5,
-                                os = "windows",
-                                nb_cores = 3)
-stop <- Sys.time()
-time_taken_TH <- stop - start
-saveRDS(SmaxN_TH, here::here("transformed_data", "SmaxN_TH_raw.rds"))
+# start <- Sys.time()
+# SmaxN_TH <- automat.maxN.spbysp(species_nm = "Thalassoma_hardwicke",
+#                                 abund_list = clean_abund_list,
+#                                 dist_df = dist_df,
+#                                 fish_speed = 1,
+#                                 os = "windows",
+#                                 nb_cores = 3)
+# stop <- Sys.time()
+# time_taken_TH <- stop - start
+# saveRDS(SmaxN_TH, here::here("transformed_data", "SmaxN_TH_raw_speed2.rds"))
 
 
 ## Species 6 - Parapercis hexophtalma:
 
-start <- Sys.time()
-SmaxN_PH <- automat.maxN.spbysp(species_nm = "Parapercis_hexophtalma",
-                                abund_list = clean_abund_list,
-                                dist_df = dist_df,
-                                fish_speed = 0.5,
-                                os = "windows",
-                                nb_cores = 3)
-stop <- Sys.time()
-time_taken_PH <- stop - start
-saveRDS(SmaxN_PH, here::here("transformed_data", "SmaxN_PH_raw.rds"))
+# start <- Sys.time()
+# SmaxN_PH <- automat.maxN.spbysp(species_nm = "Parapercis_hexophtalma",
+#                                 abund_list = clean_abund_list,
+#                                 dist_df = dist_df,
+#                                 fish_speed = 1,
+#                                 os = "windows",
+#                                 nb_cores = 3)
+# stop <- Sys.time()
+# time_taken_PH <- stop - start
+# saveRDS(SmaxN_PH, here::here("transformed_data", "SmaxN_PH_raw_speed2.rds"))
 
 ## 2 - Plot ####
 
 
-maxN_speed1 <- maxN_all_df_speed1
-maxN_speed2 <- maxN_all_df_speed2
+# call data speed 0.5:
+SmaxN_AcCten <- readRDS(here::here("transformed_data", "SmaxN_AcCten_raw.rds"))
+SmaxN_CT <- readRDS(here::here("transformed_data", "SmaxN_CT_raw.rds"))
+SmaxN_GC <- readRDS(here::here("transformed_data", "SmaxN_GC_raw.rds"))
+SmaxN_PM <- readRDS(here::here("transformed_data", "SmaxN_PM_raw.rds"))
+SmaxN_TH <- readRDS(here::here("transformed_data", "SmaxN_TH_raw.rds"))
+SmaxN_PH <- readRDS(here::here("transformed_data", "SmaxN_PH_raw.rds"))
+
+# call data speed 1:
+SmaxN_AcCten_speed2 <- readRDS(here::here("transformed_data", "SmaxN_AcCten_raw_speed2.rds"))
+SmaxN_CT_speed2 <- readRDS(here::here("transformed_data", "SmaxN_CT_raw_speed2.rds"))
+SmaxN_GC_speed2 <- readRDS(here::here("transformed_data", "SmaxN_GC_raw_speed2.rds"))
+SmaxN_PM_speed2 <- readRDS(here::here("transformed_data", "SmaxN_PM_raw_speed2.rds"))
+SmaxN_TH_speed2 <- readRDS(here::here("transformed_data", "SmaxN_TH_raw_speed2.rds"))
+SmaxN_PH_speed2 <- readRDS(here::here("transformed_data", "SmaxN_PH_raw_speed2.rds"))
+
+
+# gather data for all species - speed 0.5m/s:
+all_sp_list <- list(SmaxN_AcCten, SmaxN_CT, SmaxN_GC, SmaxN_PH, SmaxN_PM, SmaxN_TH)
+names(all_sp_list) <- c("AcCten_dark", "Chaetodon_trifasciatus",
+                        "Gomphosus_caeruleus", "Parapercis_hexophtalma",
+                        "Parupeneus_macronemus", "Thalassoma_hardwicke")
+
+
+# gather data for all species - speed 1m/s:
+all_sp_list_speed2 <- list(SmaxN_AcCten_speed2, SmaxN_CT_speed2, SmaxN_GC_speed2, SmaxN_PH_speed2,
+                    SmaxN_PM_speed2, SmaxN_TH_speed2)
+names(all_sp_list_speed2) <- c("AcCten_dark", "Chaetodon_trifasciatus",
+                        "Gomphosus_caeruleus", "Parapercis_hexophtalma",
+                        "Parupeneus_macronemus", "Thalassoma_hardwicke")
+
+
+# to a clean table - speed 0.5m/s:
+SmaxN_df_all_sp <- clean.df.maxN(all_sp_list = all_sp_list)
+
+# correct because bug for AcCten_dark, using wrong df but got data through combcam and check:
+# so I correct manually while the SmaxN_ActCten rerun:
+SmaxN_df_all_sp$SmaxN[c(1,2,3)] <- c(16,16,15)
+SmaxN_df_all_sp$maxN[c(1,2,3)] <- c(4,4,5)
+SmaxN_df_all_sp$SmaxN_timestep[c(1,2,3)] <- c(10,10,10)
+
+saveRDS(SmaxN_df_all_sp, here::here("transformed_data", "SmaxN_df_all_sp.rds"))
+
+
+
+# to a clean table - speed 1m/s:
+SmaxN_df_all_sp_speed2 <- clean.df.maxN(all_sp_list = all_sp_list_speed2)
+saveRDS(SmaxN_df_all_sp_speed2, here::here("transformed_data", "SmaxN_df_all_sp_speed2.rds"))
+
+
+
+
+maxN_speed1 <- SmaxN_df_all_sp
+maxN_speed2 <- SmaxN_df_all_sp_speed2
+
 color_poses <- c("#66c2a5", "#fc8d62", "#8da0cb")
 alpha <- 0.8
 size <- 2.5
